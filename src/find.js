6790295352aa17ed5b0447a5f2fd9cebc589f14f
qianlongo <1162633511@qq.com>
2021-09-25 22:40:36 +0800

    feat: 实现find

---
+++ src/find.js
@@ -0,0 +1,29 @@
+Array.prototype.find2 = function (callback, thisCtx) {
+  if (typeof callback !== 'function') {
+    throw `${callback} is not a function`
+  }
+
+  const length = this.length
+  let i = 0
+
+  while (i < length) {
+    const value = this[ i ]
+
+    if (i in this && callback.call(thisCtx, value, i, this)) {
+      return value
+    }
+
+    i++
+  }
+
+  return undefined
+}
+
+let arr = [ 0, 1, 2, 3, 4,, 5 ]
+
+let index = arr.find(function (it, i, array) {
+  console.log(it, i, array, this)
+  return it > 3
+}, { name: '前端胖头鱼' })
+
+console.log(index)
